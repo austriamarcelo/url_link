0cee03c0eb1ac5695084e347fdda0895fb20cef5
marcelo austria <marcelo_techops@eyp.ph>
2014-01-10 13:02:54 +0800

    url_link

---
+++ lib/url_link.rb
@@ -0,0 +1,25 @@
+module UrlLink
+
+class url_link
+
+def self.format(url_link)
+  unless url_link.blank?
+   url_link.gsub( %r{(http|https)://[^\s<]+} ) do |url|
+      if url[/(?:png|jpe?g|gif|svg)$/]
+        "<img src='#{url}' />"
+      else
+        "<a href='#{url}' target='_blank'>#{url}</a> "
+      end
+    end
+  end
+end
+
+# def url_link(proper_url_link)
+#   unless html_format.blank?
+#     html_format.html_safe
+#   end
+# end
+
+end
+
+end
