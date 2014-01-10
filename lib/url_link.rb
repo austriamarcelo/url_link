e2c08c108e46ff6aaafb3931768b2ec16cc9a5e5
marcelo austria <marcelo_techops@eyp.ph>
2014-01-10 15:51:14 +0800

    Updated the version to 0.0.6

--- lib/url_link.rb
+++ lib/url_link.rb
@@ -1,20 +1,28 @@
-class UrlLink
-  def self.format(url_link)
-    unless url_link.blank?
-     url_link.gsub( %r{(http|https)://[^\s<]+} ) do |url|
-      if url[/(?:png|jpe?g|gif|svg)$/]
-        "<img src='#{url}' />"
-      else
-        "<a href='#{url}' target='_blank'>#{url}</a> "
+module UrlLink
+  module ::ActionView
+    module Helpers # :nodoc:
+      module TextHelper
+
+        def format(url_link)
+          unless url_link.blank?
+           url_link.gsub( %r{(http|https)://[^\s<]+} ) do |url|
+            if url[/(?:png|jpe?g|gif|svg)$/]
+              "<img src='#{url}' />"
+            else
+              "<a href='#{url}' target='_blank'>#{url}</a> "
+            end
+          end
+        end
       end
-     end
+
+      def url_link(html_format)
+        unless html_format.blank?
+          html_format.html_safe
+        end
+      end
+
     end
   end
 end
 
-
-# def url_link(proper_url_link)
-#   unless html_format.blank?
-#     html_format.html_safe
-#   end
-# end
+end
