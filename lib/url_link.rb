4321ee7de75b3993c3f0d1964dddbf559f8ab3ae
marcelo austria <marcelo_techops@eyp.ph>
2014-01-13 10:36:05 +0800

    Updated the version to 1.0.0

--- lib/url_link.rb
+++ lib/url_link.rb
@@ -3,9 +3,14 @@ module UrlLink
     module Helpers # :nodoc:
       module TextHelper
 
-        def format(url_link)
-          unless url_link.blank?
-           url_link.gsub( %r{(http|https)://[^\s<]+} ) do |url|
+       def proper_url_link(url_link)
+        unless url_link.blank?
+          
+          url_link.gsub( /\s*([^\s]+@[^\s]+\.[^\s]+)\s*/ ) do |email_to|
+            "<a href='mailto:#{email_to}'>#{email_to}</a>"
+          end
+
+          url_link.gsub( %r{(http|https)://[^\s<]+} ) do |url|
             if url[/(?:png|jpe?g|gif|svg)$/]
               "<img src='#{url}' />"
             else
@@ -15,14 +20,29 @@ module UrlLink
         end
       end
 
-      def url_link(html_format)
+      def proper_email(url_link)
+        unless url_link.blank?
+          url_link.gsub( /\s*([^\s]+@[^\s]+\.[^\s]+)\s*/ ) do |email_to|
+            "<a href='mailto:#{email_to}'>#{email_to}</a>"
+          end
+        end
+      end
+
+      def proper_html(html_format)
         unless html_format.blank?
           html_format.html_safe
         end
       end
 
-    end
-  end
+      def url_link(url_link_format)
+        unless url_link_format.blank?
+         proper_html(proper_email(proper_url_link(url_link_format)))
+        end
+      end
+
+
+   end
+ end
 end
 
 end
--- url_link.gemspec
+++ url_link.gemspec
@@ -1,9 +1,9 @@
 Gem::Specification.new do |s|
   s.name = 'url_link'
-  s.version = '0.0.6'
-  s.date = '2014-01-10'
+  s.version = '1.0.0'
+  s.date = '2014-01-13'
   s.summary = "Convert all url into link"
-  s.description = 'Convert all the urls from text or string to Url https://www.facebook.com/marcelo.austria.1 <a target="_blank" https://www.facebook.com/marcelo.austria.1">https://www.facebook.com/marcelo.austria.1</a>, http://blogspot.com/images/screenshot.png  <img src="http://blogspot.com/images/screenshot.png"/>")'
+  s.description = 'Convert all the urls, img url and email from text or string to Url Link (https://www.facebook.com/marcelo.austria.1 to => <a target="_blank" https://www.facebook.com/marcelo.austria.1">https://www.facebook.com/marcelo.austria.1</a>, http://blogspot.com/images/screenshot.png  to => <img src="http://blogspot.com/images/screenshot.png"/>", marceloaustria@yahoo.com to => <a href="mailto:marceloaustria@yahoo.com">marceloaustria@yahoo.com</a>)'
   s.authors = ["Marcelo Austria"]
   s.email = 'marceloaustria@yahoo.com'
   s.files = ["lib/url_link.rb"]
